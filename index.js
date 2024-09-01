f550b39b00f350793dee0fbcd9f26132e4d0ffa4
TToth h <toth.kristof.laszlo@gmail.com>
2024-09-01 23:28:52 +0000

    modified:   index.js

--- index.js
+++ index.js
@@ -1,52 +1,117 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const bodyparser = require('body-parser');
 const mongoose = require('mongoose');
-const cors = require('cors')
-require('dotenv').config()
+const cors = require('cors');
+require('dotenv').config();
 
-app.use(cors())
-app.use(express.static('public'))
+app.use(cors());
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/views/index.html')
+	res.sendFile(__dirname + '/views/index.html');
 });
+
+mongoose.createConnection(env.MONGODB_URL);
 
 const UserModel = mongoose.model('User', new mongoose.Schema({ username: { type: String, required: true } }));
 const ExerciseModel = mongoose.model('Exercise', new mongoose.Schema({
 	username: String,
 	description: String,
 	duration: Number,
-	date: Date
+	date: Date,
+	id: String
 }));
 
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 
 app.get("/api/users", function(req, res, next) {
+	UserModel.find().select("username, _id").exec((err, data) => {
+		if(err) {
+			res.json({"error": "user not found"});
+			return;
+		}
 
+		res.json({data});
+	});
 });
 
 app.post("/api/users", async function(req, res, next) {
 	let user = req.body.username;
 
-	await UserModel.create({username: user})
+	await UserModel.create({username: user});
 
-	UserModel.find({username: user}).select("username _id").exec(function(err, data) {
+	UserModel.find({username: user}).select("username _id").exec((err, data) => {
 		if(err) {
-			res.json({"error": "user not found"})
+			res.json({"error": "user not found"});
 			return;
 		}
 
 		res.json({"username": data.username, "_id": data._id});
+	});
+});
+
+app.post("/api/users/:_id/exercises", async function(req, res, next) {
+	let id = req.body._id;
+	let user = {}
+	let exercise = {}
+
+	await ExerciseModel.create({
+		username: req.body.username,
+		description: req.body.description,
+		duration: req.body.duration,
+		date: ((!req.body.date) ? new Date().toDateString() : req.body.date.toDateString()),
+		id: req.body._id
+	});
+
+	await UserModel.find({"id": id}).exec((err, data) => {
+		if(err) {
+			res.json({"error": "user not found"});
+			return;
+		}
+
+		user = data;
+	});
+
+	res.json({
+		"username": user.username,
+		"description": req.body.description,
+		"duration": req.body.duration,
+		"date": req.body.date.toDateString(),
+		"_id": id
 	})
 });
 
-app.post("/api/users/:_id/exercises", function(req, res, next) {
+app.get("/api/users/:_id/logs", async function(req, res, next) {
+	let user = "";
+	let id = req.params._id;
+	let exercises = [];
 
-});
+	await UserModel.find({"id": req.params._id}).exec((err, data) => {
+		if(err) {
+			res.json({"error": "user not found"});
+			return;
+		}
 
-app.get("/api/users/:_id/logs", function(req, res, next) {
+		user = data.username;
+	});
 
+	await ExerciseModel.find({"id": id}).select("description duration date").exec((err, data) => {
+		if(err) {
+			res.json({"error": "user not found"});
+			return;
+		}
+
+		exercises = data;
+	});
+
+	res.json({
+		"username": user,
+		"count": exercises.length,
+		"_id": id,
+		"log": [exercises]
+	});
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
