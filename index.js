aea73cc5e6fcab26ac25cb0d89e0cd95177a1c61
TToth h <toth.kristof.laszlo@gmail.com>
2024-09-02 14:09:33 +0000

    modified:   index.js 	modified:   package-lock.json 	modified:   package.json

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var express = require('express');
+const multer  = require('multer');
+const fs = require('fs');
+const upload = multer({ dest: 'uploads/' })
+const bodyparser = require('body-parser');
 var cors = require('cors');
 require('dotenv').config()
 
@@ -8,13 +12,31 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+	res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse", upload.single('upfile'), (req, res, next) => {
+	let file = req.file;
 
+	let fName = file.originalname;
+	let fMime = file.mimetype;
+	let fSize = file.size;
 
+	fs.unlink(file.path, (err) => {
+		if (err) {
+		  console.error(`Error removing file: ${err}`);
+		  return;
+		}
+	});
+
+	res.json({
+		"name": fName,
+		"type": fMime,
+		"size": fSize
+	});
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+	console.log('Your app is listening on port ' + port)
 });
